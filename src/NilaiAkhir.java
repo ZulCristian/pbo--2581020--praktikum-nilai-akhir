0d3208879fd05ad56883426b944cb68a7a82f517
Zul Cristian Purba <2581020@unai.edu>
2026-09-25 10:33:41 +0700

    menambahkan bobot nilai

---
+++ src/NilaiAkhir.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class KalkulatorNilaiAkhir {
+    public static void main(String[] args) {
+        final double BOBOT_PRAKTIKUM = 0.30;
+        final double BOBOT_TUGAS = 0.20;
+        final double BOBOT_MID = 0.20;
+        final double BOBOT_FINAL = 0.30;
